a7974a7f7ca8b133d23c01839e7f6d29ea7cdc58
Victor Recabarren <victormrecabarren@gmail.com>
2019-08-23 17:57:33 -0400

    trying to fix scroll of shopping cart within swipe up component

--- components/ShoppingCart.js
+++ components/ShoppingCart.js
@@ -4,9 +4,11 @@ import {
   StyleSheet,
   ScrollView,
   View,
+  Button,
   Text,
   Image,
   StatusBar,
+  TouchableHighlight,
 } from 'react-native'
 
 class ShoppingCart extends Component {
@@ -16,66 +18,213 @@ class ShoppingCart extends Component {
         <Text
           style={{fontSize: 40, textAlign: "center", paddingBottom: 50, color: "brown"}}
           >Your cart</Text>
+        <View style={{height: 500}}>
           <ScrollView
-            style={{display: "flex", flexDirection: "column"}}
+            style={{display: "flex", flexDirection: "column", borderWidth: 1, borderColor: "red"}}
             >
             {
               this.props.cartItems.length
               ?
               this.props.cartItems.map(item=>(
-                 item.mini?
+                 item.mini && item.big
+                 ?
+                 <>
                   <View
                   key={item.id}
                   style={styles.cartItem}>
-                  <View>
-                    <Image
-                      style={{height: 130, width: 130}}
-                      source={{url: item.image2}}
-                     />
+                    <View>
+                      <Image
+                        style={{height: 130, width: 130}}
+                        source={{url: item.image2}}
+                       />
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", width: 150, paddingTop: 20}}>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 15}}
+                        >
+                        Mini
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown"}}
+                        >
+                        {item.name}
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown", paddingBottom: 10}}
+                        >
+                        Cupcakes
+                      </Text>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 16, fontStyle:"italic"}}
+                        >
+                        {item.mini_price}
+                      </Text>
+                    </View>
+
+
+                    <View style={{display: "flex", flexDirection: "column", justifyContent: "space-around", paddingTop: 30, paddingBottom: 30}}>
+                        <Text>
+                          -
+                        </Text>
+                        <Text>
+                          {item.mini}
+                        </Text>
+                        <Text>
+                          +
+                        </Text>
+                    </View>
                   </View>
 
-                  <View style={{display: "flex", flexDirection: "column", width: 150, paddingTop: 20}}>
-                    <Text
-                      style={{color: "#rgba(256, 92, 101, 1)", fontSize: 15}}
-                      >
-                      Mini
-                    </Text>
-                    <Text
-                      style={{fontSize: 20, color: "brown"}}
-                      >
-                      {item.name}
-                    </Text>
-                    <Text
-                      style={{fontSize: 20, color: "brown", paddingBottom: 10}}
-                      >
-                      Cupcakes
-                    </Text>
-                    <Text
-                      style={{color: "#rgba(256, 92, 101, 1)", fontSize: 16, fontStyle:"italic"}}
-                      >
-                      {item.price}
-                    </Text>
+                  <View
+                  key={item.id + "Big"}
+                  style={styles.cartItem}>
+                    <View>
+                      <Image
+                        style={{height: 130, width: 130}}
+                        source={{url: item.image}}
+                       />
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", width: 150, paddingTop: 20}}>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 15}}
+                        >
+                        Big
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown"}}
+                        >
+                        {item.name}
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown", paddingBottom: 10}}
+                        >
+                        Cupcakes
+                      </Text>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 16, fontStyle:"italic"}}
+                        >
+                        {item.price}
+                      </Text>
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", justifyContent: "space-around", paddingTop: 30, paddingBottom: 30}}>
+                      <Text>
+                        -
+                      </Text>
+                      <Text>
+                        {item.big}
+                      </Text>
+                      <Text>
+                        +
+                      </Text>
+                    </View>
+                  </View>
+                </>
+                  :
+                  item.mini
+                  ?
+                  <View
+                  key={item.id}
+                  style={styles.cartItem}>
+                    <View>
+                      <Image
+                        style={{height: 130, width: 130}}
+                        source={{url: item.image2}}
+                       />
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", width: 150, paddingTop: 20}}>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 15}}
+                        >
+                        Mini
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown"}}
+                        >
+                        {item.name}
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown", paddingBottom: 10}}
+                        >
+                        Cupcakes
+                      </Text>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 16, fontStyle:"italic"}}
+                        >
+                        {item.mini_price}
+                      </Text>
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", justifyContent: "space-around", paddingTop: 30, paddingBottom: 30}}>
+                      <Text>
+                        -
+                      </Text>
+                      <Text>
+                        {item.mini}
+                      </Text>
+                      <Text>
+                        +
+                      </Text>
+                    </View>
+                  </View>
+                  :
+                  <View
+                  key={item.id}
+                  style={styles.cartItem}>
+                    <View>
+                      <Image
+                        style={{height: 130, width: 130}}
+                        source={{url: item.image}}
+                       />
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", width: 150, paddingTop: 20}}>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 15}}
+                        >
+                        Big
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown"}}
+                        >
+                        {item.name}
+                      </Text>
+                      <Text
+                        style={{fontSize: 20, color: "brown", paddingBottom: 10}}
+                        >
+                        Cupcakes
+                      </Text>
+                      <Text
+                        style={{color: "#rgba(256, 92, 101, 1)", fontSize: 16, fontStyle:"italic"}}
+                        >
+                        {item.price}
+                      </Text>
+                    </View>
+
+                    <View style={{display: "flex", flexDirection: "column", justifyContent: "space-around", paddingTop: 30, paddingBottom: 30}}>
+                      <Text>
+                        -
+                      </Text>
+                      <Text>
+                        {item.big}
+                      </Text>
+                      <Text>
+                        +
+                      </Text>
+                    </View>
                   </View>
 
-                  <View style={{display: "flex", flexDirection: "column", justifyContent: "space-around", paddingTop: 30, paddingBottom: 30}}>
-                    <Text>
-                      -
-                    </Text>
-                    <Text>
-                      {item.mini}
-                    </Text>
-                    <Text>
-                      +
-                    </Text>
-                  </View>
-                </View>
-                : null
 
               ))
               :
               <Text>Add items to your cart to check out!</Text>
             }
           </ScrollView>
+        </View>
+          <Button style={{borderWidth: 2, borderColor: "red", height: 200, width: "100%"}} title="ok"/>
       </View>
     )
   }
@@ -96,7 +245,8 @@ const styles = StyleSheet.create({
   cartItem: {
     display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between"
+    justifyContent: "space-between",
+    paddingBottom: 30,
   }
 })
 
